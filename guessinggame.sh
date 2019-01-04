ade1fc0e553ad0b8f45851ae49e8f7e32b987be9
sara1305 <45824334+sara1305@users.noreply.github.com>
2019-01-04 20:59:50 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,3 +1,10 @@
 echo "Please Guess Number of files in my Github Repository"
 read number
 echo "You entered:$number"
+if [[ $number -eq 3 ]] 
+then 
+     echo "Congrats. You guessed right"
+else
+     echo "Sorry. Wrong Guess"
+     
+fi
